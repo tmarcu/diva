cf0b2984b1d4fb8b8eb90392d1c283537ff5c4a2
Matthew Johnson <matthew.johnson@intel.com>
2018-06-04 11:22:04 -0700

    Add Results definition and functions
    Add a basic Results type that enables printing in different formats.
    This new internal testutils package can be expanded to provide more
    test-specific helper functions.
    
    This allows users of the API to call r.Print<format> to print the test
    results struct in a consistent manner.
    
    Signed-off-by: Matthew Johnson <matthew.johnson@intel.com>

---
+++ internal/testutils/results.go
@@ -0,0 +1,63 @@
+// Copyright © 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package testutils
+
+import (
+	"encoding/json"
+	"io"
+	"text/template"
+)
+
+// Results holds the results of a test run
+type Results struct {
+	Passed   uint
+	Failed   uint
+	Skipped  uint
+	Total    uint
+	Failures []string
+}
+
+const resultTemplate = `
+Total:    {{.Total}}
+Passed:   {{.Passed}}
+Failed:   {{.Failed}}
+Skipped:  {{.Skipped}}
+
+{{range $f := .Failures}}
+{{.f}}
+{{end}}
+`
+
+// Print the Results to the Writer provided using the resultTemplate as the
+// format.
+func (r *Results) Print(w io.Writer) error {
+	t, err := template.New("resultTemplate").Parse(resultTemplate)
+	if err != nil {
+		return err
+	}
+
+	return t.Execute(w, r)
+}
+
+// PrintJSON prints the Results in JSON format to the Writer provided.
+func (r *Results) PrintJSON(w io.Writer) error {
+	resOut, err := json.Marshal(r)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.WriteString(w, string(resOut))
+	return err
+}
