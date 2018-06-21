4666ed57f0233ecd8390cc7b9d8f9672864728a7
gabrielle beyer <gabrielle.n.beyer@intel.com>
2018-06-21 14:50:46 -0700

    Bundle definition library
    A library that reads bundle/pundle information and creates
    bundle definitions. A single bundle/bundle, or all bundles
    within a clr-bundles directory can be constructed. The
    definition has header information, include dependencies,
    direct package dependencies, and all package dependencies.
    
    Signed-off-by: gabrielle beyer <gabrielle.n.beyer@intel.com>

---
+++ bundle/bundle.go
@@ -0,0 +1,274 @@
+// Copyright © 2018 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bundle
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+// Global variable to store os-core bundle definition
+var coreBundle = &Definition{}
+
+// Header is a struct that contains bundle header information.
+type Header struct {
+	Title        string
+	Description  string
+	Status       string
+	Capabilities string
+	Maintainer   string
+}
+
+// Definition stores bundle and pundle information. This includes the
+// name, Header information, a set of bundle includes, a set of direct
+// packages, and a set of all packages.
+type Definition struct {
+	Name   string
+	Header Header
+
+	Includes       map[string]bool
+	DirectPackages map[string]bool
+	AllPackages    map[string]bool
+}
+
+// Set is a map of bundle names to their definition
+type Set map[string]*Definition
+
+// Implement the global os-core bundle definition, that is used by all bundles
+func initializeOsCore(bundlesDir string) error {
+	var err error
+	coreBundle, err = getBundleDefinition("os-core", bundlesDir, make(map[string]bool))
+	return err
+}
+
+func newDefinition(name, bundlesDir string) (Definition, error) {
+	// The os-core bundle must exist, and be incorporated into all bunde definitions
+	if name != "os-core" && reflect.DeepEqual(coreBundle, &Definition{}) {
+		if err := initializeOsCore(bundlesDir); err != nil {
+			return Definition{}, err
+		}
+	}
+
+	b := Definition{
+		Includes:       make(map[string]bool),
+		DirectPackages: make(map[string]bool),
+		AllPackages:    make(map[string]bool),
+	}
+
+	b.Name = name
+	b.Includes[name] = true
+
+	for include := range coreBundle.Includes {
+		b.Includes[include] = true
+	}
+	for pkg := range coreBundle.AllPackages {
+		b.AllPackages[pkg] = true
+	}
+
+	return b, nil
+}
+
+func updateIncludes(packageInclude, bundlesDir string, b *Definition, visitedIncludes map[string]bool) error {
+	if _, exists := visitedIncludes[packageInclude]; exists {
+		return errors.New("Bundle include loop detected")
+	}
+
+	visitedIncludes[packageInclude] = true
+
+	include, err := getBundleDefinition(packageInclude, bundlesDir, visitedIncludes)
+	if err != nil {
+		return err
+	}
+
+	b.Includes[packageInclude] = true
+	for inc := range include.Includes {
+		b.Includes[inc] = true
+	}
+	for pkg := range include.AllPackages {
+		b.AllPackages[pkg] = true
+	}
+
+	return nil
+}
+
+func readContent(name, bundlesDir string, b *Definition, visitedIncludes map[string]bool) (*Definition, error) {
+	content, err := ioutil.ReadFile(filepath.Join(bundlesDir, "bundles", name))
+	if err != nil {
+		return nil, err
+	}
+
+	bundleHeaderFieldRegex := regexp.MustCompile(`^# \[([A-Z]+)\]:\s*(.*)$`)
+	includeBundleRegex := regexp.MustCompile(`^include\(([A-Za-z0-9_-]+)\)$`)
+
+	for _, line := range strings.Split(string(content), "\n") {
+		if matches := bundleHeaderFieldRegex.FindStringSubmatch(line); len(matches) > 2 {
+			key := matches[1]
+			value := strings.TrimSpace(matches[2])
+			switch key {
+			case "TITLE":
+				b.Header.Title = value
+			case "DESCRIPTION":
+				b.Header.Description = value
+			case "STATUS":
+				b.Header.Status = value
+			case "CAPABILITIES":
+				b.Header.Capabilities = value
+			case "MAINTAINER":
+				b.Header.Maintainer = value
+			default:
+				return nil, fmt.Errorf("Unknown header option found")
+			}
+			continue
+		} else if matches := includeBundleRegex.FindStringSubmatch(line); len(matches) > 1 {
+			err := updateIncludes(matches[1], bundlesDir, b, visitedIncludes)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			if strings.TrimSpace(line) != "" && !strings.HasPrefix(line, "#") {
+				b.DirectPackages[line] = true
+				b.AllPackages[line] = true
+			}
+		}
+	} // end reading file
+	return b, nil
+}
+
+func checkIfPundle(name string, pundles string) bool {
+	for _, line := range strings.Split(pundles, "\n") {
+		if strings.EqualFold(line, name) {
+			return true
+		}
+	}
+	return false
+}
+
+func getPundleDefinition(name string, pundle *Definition) (*Definition, error) {
+	pundle.Header.Title = name
+	pundle.DirectPackages[name] = true
+	pundle.AllPackages[name] = true
+	return pundle, nil
+}
+
+func getBundleDefinition(name, bundlesDir string, visitedIncludes map[string]bool) (*Definition, error) {
+	b, err := newDefinition(name, bundlesDir)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = os.Stat(filepath.Join(bundlesDir, "bundles", name)); os.IsNotExist(err) {
+		pundles, err := ioutil.ReadFile(filepath.Join(bundlesDir, "packages"))
+		if err != nil {
+			return nil, err
+		}
+
+		if isPundle := checkIfPundle(name, string(pundles)); isPundle {
+			return getPundleDefinition(name, &b)
+		}
+		return nil, fmt.Errorf("%s is neither a pundle nor a bundle", name)
+	}
+	return readContent(name, bundlesDir, &b, visitedIncludes)
+}
+
+// GetDefinition reads the bundle definition from the bundlesDir repository and
+// returns a *Definition of that bundle
+func GetDefinition(name, bundlesDir string) (*Definition, error) {
+	return getBundleDefinition(name, bundlesDir, make(map[string]bool))
+}
+
+// GetAll reads all bundle definitions in the bundlesDir repository and returns a
+// map[string]*Definition of bundle names to their definition structs.
+func GetAll(bundlesDir string) (Set, error) {
+	bundles := make(Set)
+	err := filepath.Walk(filepath.Join(bundlesDir, "bundles"), func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+		bundle, err := GetDefinition(info.Name(), bundlesDir)
+		if err != nil {
+			return err
+		}
+		bundles[info.Name()] = bundle
+		return nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	pundles, err := ioutil.ReadFile(filepath.Join(bundlesDir, "packages"))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, line := range strings.Split(string(pundles), "\n") {
+		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		p, err := newDefinition(line, bundlesDir)
+		if err != nil {
+			return nil, err
+		}
+
+		pundle, err := getPundleDefinition(line, &p)
+		if err != nil {
+			return nil, err
+		}
+		bundles[line] = pundle
+	}
+	return bundles, nil
+}
+
+// GetIncludesForBundle returns a sorted slice of all includes for a specified bundle.
+func GetIncludesForBundle(name, bundlesDir string) ([]string, error) {
+	bundle, err := GetDefinition(name, bundlesDir)
+	if err != nil {
+		return nil, err
+	}
+	var sorted []string
+	for p := range bundle.Includes {
+		sorted = append(sorted, p)
+	}
+	sort.Strings(sorted)
+	return sorted, nil
+}
+
+// GetAllPackagesForBundle returns a sorted slice of all packages for a specified
+// bundle. AllPackages includes the direct packages for a bundle/pundle, along
+// with the direct packages of the bundle includes; so all package dependencies.
+func GetAllPackagesForBundle(name, bundlesDir string) ([]string, error) {
+	bundle, err := GetDefinition(name, bundlesDir)
+	if err != nil {
+		return nil, err
+	}
+	var sorted []string
+	for p := range bundle.AllPackages {
+		sorted = append(sorted, p)
+	}
+	sort.Strings(sorted)
+	return sorted, nil
+}
